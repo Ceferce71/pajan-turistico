5d555c73f5806953ca414eb2fafdfd9d92a828b7
Ceferce71 <ceferce71@gmail.com>
2021-10-29 10:15:13 -0500

    primera edicion

---
+++ data/UPCs_Pajan_9.js
@@ -0,0 +1,12 @@
+var json_UPCs_Pajan_9 = {
+"type": "FeatureCollection",
+"name": "UPCs_Pajan_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nombre": "UPC PAJAN" }, "geometry": { "type": "Point", "coordinates": [ -80.430681410731722, -1.555351641590361 ] } },
+{ "type": "Feature", "properties": { "Nombre": "UPC LASCANO" }, "geometry": { "type": "Point", "coordinates": [ -80.227826749845605, -1.531953683440048 ] } },
+{ "type": "Feature", "properties": { "Nombre": "UPC GUALE" }, "geometry": { "type": "Point", "coordinates": [ -80.237857494014563, -1.628268298609045 ] } },
+{ "type": "Feature", "properties": { "Nombre": "UPC CAMPOZANO" }, "geometry": { "type": "Point", "coordinates": [ -80.397303291969976, -1.588419630722841 ] } },
+{ "type": "Feature", "properties": { "Nombre": "UPC CASCOL" }, "geometry": { "type": "Point", "coordinates": [ -80.459103688881441, -1.665073533046138 ] } }
+]
+}
